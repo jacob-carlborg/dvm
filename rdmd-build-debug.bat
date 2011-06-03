e03f48c2db0ce960b22b3c509b7d3f8c145c86b0
Abscissa <bus_github@semitwist.com>
2011-06-03 02:41:29 -0400

    Added scripts to build DVM with RDMD.

---
+++ rdmd-build-debug.bat
@@ -0,0 +1,8 @@
+@echo off
+
+REM   This requires the latest rdmd in git (from 2011-05-14 or newer):
+REM      https://github.com/D-Programming-Language/tools
+REM   Then, rdmd can be compiled with DMD 2.053
+REM   Or, you can 'dsss build' if you have dsss, but rdmd is much faster.
+
+rdmd -Jresources --build-only -ofdvm\dvm\dvm -debug dvm\dvm\dvm.d
