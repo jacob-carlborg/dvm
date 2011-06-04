bcf720c3386d61ff65bf87deccb2140f1aa29937
Abscissa <bus_github@semitwist.com>
2011-06-04 03:35:41 -0400

    Small improvements to dvm.bat

--- resources/dvm.bat
+++ resources/dvm.bat
@@ -3,13 +3,13 @@
 set dvm_prefix=%APPDATA%\
 set dvm_path=%dvm_prefix%dvm
 
-set dvm_tmp_path=%dvm_path%/tmp
-set dvm_result_path=%dvm_tmp_path%/result.bat
-set dvm_bin_path=%dvm_path%/bin
-set dvm_exe_path=%dvm_bin_path%/_dvm.exe
-set dvm_default_env_path=%dvm_path%/env/default
-set dvm_default_bin_path=%dvm_bin_path%/dvm-default-dc
-set dvm_current_path=%dvm_bin_path%/dvm-current-dc
+set dvm_tmp_path=%dvm_path%\tmp
+set dvm_result_path=%dvm_tmp_path%\result.bat
+set dvm_bin_path=%dvm_path%\bin
+set dvm_exe_path=%dvm_bin_path%\_dvm.exe
+set dvm_default_env_path=%dvm_path%\env\default
+set dvm_default_bin_path=%dvm_bin_path%\dvm-default-dc
+set dvm_current_path=%dvm_bin_path%\dvm-current-dc
 
 REM if exist "%dvm_exe_path%" (
 REM 	call "%dvm_default_env_path%"
@@ -25,9 +25,7 @@ if exist "%dvm_exe_path%" (
 
 if exist "%dvm_result_path%" (
 	call "%dvm_result_path%"
-	
-	REM Prevent result.bat from accedentally being rerun next time dvm.bat is run
-	move /Y "%dvm_result_path%" "old_%dvm_result_path%"
 )
 
-del /Q /F /S "%dvm_tmp_path%" > NUL
+rmdir /Q /S "%dvm_tmp_path%"
+mkdir "%dvm_tmp_path%"
