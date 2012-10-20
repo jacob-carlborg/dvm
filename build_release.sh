85b459f0b563ae752183c149e8ad0f09e32502d2
Jacob Carlborg <doob@me.com>
2012-10-20 22:00:18 +0200

    Use dvm in the build script, if available.

--- build_release.sh
+++ build_release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#   This requires the latest rdmd in git (from 2011-05-14 or newer):
-#      https://github.com/D-Programming-Language/tools
-#   Then, rdmd can be compiled with DMD 2.053
-#   Or, you can 'dsss build' if you have dsss, but rdmd is much faster.
+if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
+    . "$HOME/.dvm/scripts/dvm" ;
+    dvm use 1.072
+fi
 
-rdmd -Jresources -L-lz --build-only -ofbin/dvm -release -O -inline dvm/dvm/dvm.d
+rdmd -Jresources -L-lz --build-only -ofbin/dvm -release -O -inline "$@" dvm/dvm/dvm.d
