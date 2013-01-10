d7c479e814a2a0d38721ffc6aed7401b3a258445
Jacob Carlborg <doob@me.com>
2013-01-10 21:14:52 +0100

    Build Mac OS X 10.6 compatible binary.

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
     dvm use 1.072
 fi
 
-rdmd -Jresources -L-lz --build-only -ofbin/dvm -debug "$@" dvm/dvm/dvm.d
+rdmd -Jresources -L-lz --build-only -ofbin/dvm -debug -L-macosx_version_min -L10.6 "$@" dvm/dvm/dvm.d
