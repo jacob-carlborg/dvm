3027ac4ab3bb9df2304d999e33b5c7620681ec57
Jacob Carlborg <doob@me.com>
2013-12-17 21:04:19 +0100

    Reenable accidentally commented code.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
-#     . "$HOME/.dvm/scripts/dvm" ;
-#     dvm use 1.072
-# fi
+if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
+    . "$HOME/.dvm/scripts/dvm" ;
+    dvm use 1.072
+fi
 
 rdmd -Jresources -L-lz --build-only -ofbin/dvm -debug -L-macosx_version_min -L10.6 "$@" dvm/dvm/dvm.d
