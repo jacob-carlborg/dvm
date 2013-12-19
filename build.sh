2365875376a84ee37d7a7a0edc3e3918eee1a15b
Jacob Carlborg <doob@me.com>
2013-12-19 08:26:11 +0100

    Update to DMD 2.064.2.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
     . "$HOME/.dvm/scripts/dvm" ;
-    dvm use 2.064
+    dvm use 2.064.2
 fi
 
 rdmd -Imambo -Jresources -L-lz -L-ltango --build-only -ofbin/dvm -debug -gc -L-macosx_version_min -L10.6 "$@" dvm/dvm/dvm.d
