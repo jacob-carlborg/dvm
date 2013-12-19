8eea924ed64c9562238952cce44a3024bfdff22a
Jacob Carlborg <doob@me.com>
2013-12-19 21:08:40 +0100

    Only use the -macosx_version_min flag when building on Mac OS X.

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@ if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
     dvm use 2.064.2
 fi
 
-rdmd -Imambo -Jresources -L-lz -L-ltango --build-only -ofbin/dvm -debug -gc -L-macosx_version_min -L10.6 "$@" dvm/dvm/dvm.d
+if [ `uname` == "Darwin" ] ; then
+    extra_linker_flags="-L-macosx_version_min -L10.6"
+else
+    extra_linker_flags=""
+fi
+
+rdmd -Imambo -Jresources -L-lz -L-ltango --build-only -ofbin/dvm -debug -gc $extra_linker_flags "$@" dvm/dvm/dvm.d
