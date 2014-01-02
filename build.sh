dea97513867010be3c7b602bff3b2566f2b8bb0c
Jacob Carlborg <doob@me.com>
2014-01-02 14:37:33 +0100

    Spaces to tabs.

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
-    . "$HOME/.dvm/scripts/dvm" ;
-    dvm use 2.064.2
+	. "$HOME/.dvm/scripts/dvm" ;
+	dvm use 2.064.2
 fi
 
 if [ "$(uname)" = "Darwin" ] ; then
-    extra_linker_flags="-L-macosx_version_min -L10.6"
+	extra_linker_flags="-L-macosx_version_min -L10.6"
 else
-    extra_linker_flags=""
+	extra_linker_flags=""
 fi
 
 rdmd -Imambo -Jresources -L-lz -L-ltango --build-only -ofbin/dvm -debug -gc $extra_linker_flags "$@" dvm/dvm/dvm.d
